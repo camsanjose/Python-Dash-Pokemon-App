f9a7fb3804d30b799731f8c5536ec0d957649a18
camsanjose <60402417+camsanjose@users.noreply.github.com>
2020-03-25 11:56:18 +0100

    Adding background image and table

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
-#import http.client
-
-#conn = http.client.HTTPSConnection("pokemon-go1.p.rapidapi.com")
-
-#headers = {
-#    'x-rapidapi-host': "pokemon-go1.p.rapidapi.com",
-#    'x-rapidapi-key': "11a0831306msh85289b71ee29d28p1b0d2ajsnd56ec6de06bd"
-#    }
-
-#conn.request("GET", "/pokemon_stats.json", headers=headers)
-
-#res = conn.getresponse()
-#data = res.read()
-
-#print(data.decode("utf-8"))
-
-#print(type(data))
+import http.client
 import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd 
+import dash_table
 
+df_url = "https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv"
+df = pd.read_csv(df_url)
 
 app = dash.Dash(__name__)
 server = app.server
+app.title= "My Pokemon Dash App"
 
-
-app.layout = html.Div([
-    html.H2('Pokemon',style={
-            'textAlig':"center",
-            'color': 'lightblue'
-            }),
+app.layout = html.Div(style= {'background-image': 'url(https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/5b0fcfa25bafe83df2c203b3/pokemon0_0.jpg)'},
+children=[
+    html.H1('Pokemon', style={
+        'color': 'lightblue',
+        'textAlig':"center",
+    }),
+    html.Div(children=['''
+    Hello! Welcome to the Dash Pokemon app.  
+    This will help you to get to know in-depth all pokemon in hopes you can 
+    choose which one is best for you.
+    ''']),
+    html.Div(children=['''
+    There are three types of main characteristics
+    in all Pokemon: stamina, defense, and attack. 
+    ''']),
+#    html.Label('Dropdown'),
+#    dcc.Dropdown(
+#        id='my-dropdown',
+#        options=opt_vore,
+#        value=db_vore[0]
+#            ),
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name":i,"id":i} for i in df.columns],
+        data=df.to_dict('records')
+        ),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
