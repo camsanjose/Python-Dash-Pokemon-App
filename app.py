0245fbf783874baf3aea8fa3da39b0509109425f
camsanjose <60402417+camsanjose@users.noreply.github.com>
2020-03-25 13:56:28 +0100

    Adding dropdown selector for each Pokemon

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd 
 import dash_table
+from dash.dependencies import Input, Output
 
 df_url = "https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv"
 df = pd.read_csv(df_url)
+df= df[['Name', 'Type 1','Total', 'Attack','Defense', 'Speed']]
+df= df.dropna().drop_duplicates(keep= 'first')
+df= df.sort_values('Name')
+states = df['Name'].unique().tolist()
+
 
 app = dash.Dash(__name__)
 server = app.server
@@ -34,11 +40,17 @@ children=[
 #        options=opt_vore,
 #        value=db_vore[0]
 #            ),
+    html.Div(
+        children=[
+            dcc.Dropdown(
+                id='filter_dropdown',
+                options=[{'label':st, 'value':st} for st in states],
+                value = states[0]
+                ),
     dash_table.DataTable(
-        id='table',
-        columns=[{"name":i,"id":i} for i in df.columns],
-        data=df.to_dict('records')
-        ),
+        id='table-container', 
+        columns=[{'id': c, 'name': c} for c in df.columns.values])
+        ]),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
@@ -54,6 +66,12 @@ app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 
+@app.callback(
+    Output('table-container', 'data'),
+    [Input('filter_dropdown', 'value') ] )
+def display_table(states):
+    dff = df[df['Name']==states]
+    return dff.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
